1a6757b0b8a9194421093c6b9dfa2fb5d843d4ab
ajito.costa <ajito.costa@viettel.com.vn>
2023-06-09 16:34:41 +0700

    Rush into print

---
+++ Project_Zooleepr/src/Rush_into_print/ex2.java
@@ -0,0 +1,15 @@
+package Rush_into_print;
+
+public class ex2 {
+    public static void main(String[] args) {
+        // write your code here
+        String[] phrases = {"I love animals!",
+                "Let's check on the animals...",
+                "The deer looks fine.",
+                "The bat looks happy.",
+                "The lion looks healthy."};
+        for (String s: phrases) {
+            System.out.println(s);
+        }
+    }
+}
---
+++ Project_Zooleepr/src/Rush_into_print/exer_1.java
@@ -0,0 +1,16 @@
+package Rush_into_print;
+
+import java.util.Scanner;
+
+public class exer_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("I love animals!");
+        System.out.println("Let's check on the animals...");
+        System.out.println("The deer looks fine.");
+        System.out.println("The bat looks happy.");
+        System.out.println("The lion looks healthy.");
+    }
+
+}
